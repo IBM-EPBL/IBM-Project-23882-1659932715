2320d3d4c19745cc6b06787ad38ff79b80daa857
gopinathe12 <116366039+gopinathe12@users.noreply.github.com>
2022-10-21 23:00:14 +0530

    Delete 917719C127_Assignment2.py

--- 917719C127_Assignment2.py
+++
@@ -1,8 +0,0 @@
-import random
-while(True):
-    temp=random.randrange(0,100)
-    humid=random.randrange(0,100)
-    if (temp>90 or temp<80) and humid>60:
-	    print("Alert, Please Make Precautions")
-    else:
-	    print("safe with extra water bodies")
