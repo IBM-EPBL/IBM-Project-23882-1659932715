a0289654aab25a2fc3623c02c58c545cc6c35ba9
GokulAnanthM <93421814+GokulAnanthM@users.noreply.github.com>
2022-10-21 19:33:54 +0530

    Add files via upload

---
+++ 917719C106_Assignment2.py
@@ -0,0 +1,8 @@
+import random
+while(True):
+    temp=random.randrange(0,100)
+    humid=random.randrange(0,100)
+    if (temp>90 or temp<80) and humid>60:
+	    print("Alert, Please Make Precautions")
+    else:
+	    print("safe with extra water bodies")
